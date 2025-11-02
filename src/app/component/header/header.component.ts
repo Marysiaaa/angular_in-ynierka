d069cfaad68ad639623473d012f768c3ce3fd8df
Marysiaaa <115399847+Marysiaaa@users.noreply.github.com>
2025-11-02 17:55:07 +0100

    stworzenie nawigacji, i 2 podstron

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,26 +1,27 @@
-import { Component, inject } from "@angular/core";
-import { AuthService } from "../../services/auth.service";
-import { NgOptimizedImage } from "@angular/common";
-import { MatButtonModule } from "@angular/material/button";
-import { IfAuthenticatedDirective } from "../../directives/if-authenticated.directive";
-import { RoutePath } from "../../enums/route-path";
-import { RouterLink } from "@angular/router";
+import {Component, inject} from "@angular/core";
+import {AuthService} from "../../services/auth.service";
+import {NgOptimizedImage} from "@angular/common";
+import {MatButtonModule} from "@angular/material/button";
+import {IfAuthenticatedDirective} from "../../directives/if-authenticated.directive";
+import {RoutePath} from "../../enums/route-path";
+import {RouterLink} from "@angular/router";
+import {NavigationComponent} from '../navigation/navigation.component';
 
 @Component({
-    selector: "cp-header",
-    imports: [NgOptimizedImage, MatButtonModule, IfAuthenticatedDirective, RouterLink],
-    templateUrl: "./header.component.html",
-    styleUrl: "./header.component.scss"
+  selector: "cp-header",
+  imports: [NgOptimizedImage, MatButtonModule, IfAuthenticatedDirective, RouterLink, NavigationComponent],
+  templateUrl: "./header.component.html",
+  styleUrl: "./header.component.scss"
 })
 export class HeaderComponent {
-    protected readonly RoutePath: typeof RoutePath = RoutePath;
-    private readonly _authService: AuthService = inject(AuthService);
+  protected readonly RoutePath: typeof RoutePath = RoutePath;
+  private readonly _authService: AuthService = inject(AuthService);
 
-    protected async onLogoutClick(): Promise<void> {
-        await this._authService.logout();
-    }
+  protected async onLogoutClick(): Promise<void> {
+    await this._authService.logout();
+  }
 
-    protected async OnLoginClick(): Promise<void> {
-        // await this._authService.login();
-    }
+  protected async OnLoginClick(): Promise<void> {
+    // await this._authService.login();
+  }
 }
